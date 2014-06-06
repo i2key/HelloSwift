4c45e59058b7f8b36ae2e4b842cc99a592e365eb
i2key <itsuki.k@gmail.com>
2014-06-06 13:38:06 +0900

    Initial Commit

---
+++ HelloSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloSwift
+//
+//  Created by 黒田 樹 on 2014/06/06.
+//  Copyright (c) 2014年 i2key. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
